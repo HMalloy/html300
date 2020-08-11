59b8ca680bc66f30920e41e5928210c525a1e92c
Heather Malloy <hmalloy@bluetooth.com>
2020-08-11 09:36:04 -0700

    fixed v-if v-else in App.vue

--- lesson07/assignment/met-art/src/App.vue
+++ lesson07/assignment/met-art/src/App.vue
@@ -6,23 +6,6 @@
       <b-container fluid class="p-5">
         <!-- show this layout if you are on any page that is not the home page -->
         <!-- I don't know if this is the best use of v-if, but it is what I could think of to try  -->
-        <div v-if="!home">
-          <b-row class="d-flex justify-content-between">
-            <!-- main content -->
-            <b-col class="col-12 col-sm-10 col-md-8 col-lg-6">
-              <router-view/>
-            </b-col>
-            <!-- shared sidebar -->
-            <b-col class="col-12 col-md-4 mt-3 mt-md-0">
-              <!-- featured art -->
-              <FeaturedArt/>
-              <!-- latest blog post -->
-              <LatestBlog/>
-              <!-- Upcoming Event -->
-              <Events/>
-            </b-col>
-          </b-row>
-        </div>
         <!-- show this layout if you are on the home page -->
         <div v-if="home">
           <router-view/>
@@ -36,6 +19,23 @@
               <LatestBlog/>
             </b-col>
             <b-col md="4">
+              <!-- Upcoming Event -->
+              <Events/>
+            </b-col>
+          </b-row>
+        </div>
+        <div v-else>
+          <b-row class="d-flex justify-content-between">
+            <!-- main content -->
+            <b-col class="col-12 col-sm-10 col-md-8 col-lg-6">
+              <router-view/>
+            </b-col>
+            <!-- shared sidebar -->
+            <b-col class="col-12 col-md-4 mt-3 mt-md-0">
+              <!-- featured art -->
+              <FeaturedArt/>
+              <!-- latest blog post -->
+              <LatestBlog/>
               <!-- Upcoming Event -->
               <Events/>
             </b-col>
