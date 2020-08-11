665a8fb71da41c6f475b77f04e320ce6693f778d
Heather Malloy <hmalloy@bluetooth.com>
2020-08-11 08:53:38 -0700

    assignment complete

---
+++ lesson07/assignment/met-art/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div id="app">
+    <b-container fluid class="p-0">
+      <!-- shared main nav -->
+      <MainNav/>
+      <b-container fluid class="p-5">
+        <!-- show this layout if you are on any page that is not the home page -->
+        <!-- I don't know if this is the best use of v-if, but it is what I could think of to try  -->
+        <div v-if="!home">
+          <b-row class="d-flex justify-content-between">
+            <!-- main content -->
+            <b-col class="col-12 col-sm-10 col-md-8 col-lg-6">
+              <router-view/>
+            </b-col>
+            <!-- shared sidebar -->
+            <b-col class="col-12 col-md-4 mt-3 mt-md-0">
+              <!-- featured art -->
+              <FeaturedArt/>
+              <!-- latest blog post -->
+              <LatestBlog/>
+              <!-- Upcoming Event -->
+              <Events/>
+            </b-col>
+          </b-row>
+        </div>
+        <!-- show this layout if you are on the home page -->
+        <div v-if="home">
+          <router-view/>
+          <b-row>
+            <b-col md="4" class="pt-5">
+              <!-- featured art -->
+              <FeaturedArt/>
+            </b-col>
+            <b-col md="4">
+              <!-- latest blog post -->
+              <LatestBlog/>
+            </b-col>
+            <b-col md="4">
+              <!-- Upcoming Event -->
+              <Events/>
+            </b-col>
+          </b-row>
+        </div>
+      </b-container>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import MainNav from "./components/MainNav";
+import FeaturedArt from "./components/FeaturedArt";
+import LatestBlog from "./components/LatestBlog";
+import Events from "./components/Events";
+export default {
+  name: 'App',
+  components: {MainNav, FeaturedArt, LatestBlog, Events},
+  computed:{
+    home(){
+      return this.$route.path ==='/'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ lesson07/assignment/met-art/src/components/Contact.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <b-alert v-model="messageSubmitted" variant="success" dismissible>Your message has been received!</b-alert>
+    <b-form>
+      <b-row>
+        <b-col md="6">
+          <b-form-group id="firstName" label="First Name" label-for="firstNameInput">
+            <b-form-input
+              id="firstNameInput"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col md="6">
+          <b-form-group id="lastName" label="First Name" label-for="lastNameInput">
+            <b-form-input
+              id="lastNameInput"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-row>
+      <b-form-group
+        id="emailAddress"
+        label="Email address"
+        label-for="emailAddresInput"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="emailAddresInput"
+          type="email"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="mainMessage"
+        label="Message"
+      >
+        <small class="text-secondary">What can we help you with today?</small>
+        <b-form-textarea
+          id="mainMessageInput"
+          v-model="text"
+          rows="3"
+        ></b-form-textarea>
+      </b-form-group>
+      <b-form-checkbox
+        id="newsletter"
+        name="checkbox-1"
+        value="yes"
+        unchecked-value="no"
+      >
+        Sign me up for a monthly newsletter!
+      </b-form-checkbox>
+
+      <b-button @click="messageSubmitted=true" class="mt-3" type="button" variant="dark">Submit</b-button>
+      <small id="submitHelp" class="form-text text-muted">You will receive a response within 2 business days.</small>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  data () {
+    return {
+      msg: 'Contact',
+      messageSubmitted: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
